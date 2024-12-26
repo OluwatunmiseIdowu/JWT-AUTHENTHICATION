2da839e40963276f9afec04ea3ab2c1b0a0aedb5
OluwatunmiseIdowu <144564830+OluwatunmiseIdowu@users.noreply.github.com>
2024-12-26 21:12:22 +0100

    Update auth.middleware.ts

--- src/shared/middleware/auth.middleware.ts
+++ src/shared/middleware/auth.middleware.ts
@@ -3,10 +3,9 @@ import jwt from 'jsonwebtoken';
 import logger from '../utils/logger/logger';
 
 interface UserPayload {
-  id: string; // Define other properties as needed
+  id: string; // You can add other properties as needed
 }
 
-// Middleware stays the same
 export const authMiddleware = (
   req: Request,
   res: Response,
@@ -16,24 +15,17 @@ export const authMiddleware = (
 
   if (!token) {
     logger.warn('Access denied. No token provided.');
-    return res
-      .status(401)
-      .json({ message: 'Access denied. No token provided.' });
+    return res.status(401).json({ message: 'Access denied. No token provided.' });
   }
 
   try {
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET as string
-    ) as UserPayload;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as UserPayload;
 
-    (req as any).user = decoded;
+    req.user = decoded; // TypeScript knows that req.user is a UserPayload
     logger.info('User authenticated successfully.');
     next();
   } catch (error) {
-    logger.error(
-      `Invalid token: ${error instanceof Error ? error.message : error}`
-    );
+    logger.error(`Invalid token: ${error instanceof Error ? error.message : error}`);
     return res.status(400).json({ message: 'Invalid token.' });
   }
 };
